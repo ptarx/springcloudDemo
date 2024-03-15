f9c6b16e4228a1f1b0599abb68ce9421163a711d
zxx <1508790114@qq.com>
2024-03-15 19:32:55 +0800

    提交

--- cloud-provider-payment8001/src/main/java/top/ptarx/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/top/ptarx/controller/PayController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 import top.ptarx.entities.Pay;
 import top.ptarx.entities.PayDTO;
+import top.ptarx.resp.ResultData;
 import top.ptarx.service.PayService;
 
 import java.util.List;
@@ -22,33 +23,38 @@ public class PayController {
 
     @PostMapping(value="add")
     @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
-    public Integer addPay(@RequestBody Pay pay){
-        return payService.add(pay);
+    public ResultData addPay(@RequestBody Pay pay){
+        int add = payService.add(pay);
+        return ResultData.success(add);
     }
 
     @PostMapping(value="del/{id}")
     @Operation(summary = "删除",description = "删除支付流水方法")
-    public Integer deletePay(@PathVariable("id") Integer id){
-        return payService.delete(id);
+    public ResultData deletePay(@PathVariable("id") Integer id){
+        int delete = payService.delete(id);
+        return ResultData.success(delete);
     }
 
     @GetMapping(value="getById/{id}")
     @Operation(summary = "详情",description = "支付流水详情方法")
-    public Pay getById(@PathVariable("id") Integer id){
-        return payService.getById(id);
+    public ResultData getById(@PathVariable("id") Integer id){
+        Pay pay= payService.getById(id);
+        return ResultData.success(pay);
     }
 
     @PutMapping(value="update")
     @Operation(summary = "更新",description = "更新支付流水方法")
-    public Integer update(@RequestBody PayDTO pay){
+    public ResultData update(@RequestBody PayDTO pay){
         Pay payReal = new Pay();
         BeanUtils.copyProperties(pay,payReal);
-        return payService.update(payReal);
+        payService.update(payReal);
+        return ResultData.success("更新成功");
     }
 
     @GetMapping(value="selectAll")
     @Operation(summary = "列表",description = "支付流水列表")
-    public List<Pay> selectAll(){
-        return payService.getAll();
+    public ResultData selectAll(){
+        List<Pay> all = payService.getAll();
+        return ResultData.success(all);
     }
 }
